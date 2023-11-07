9e0a959aad8c60913da35fd5954d68fafb18e8f0
JohnCardona521 <147522511+JohnCardona521@users.noreply.github.com>
2023-11-07 08:38:23 -0500

    Add files via upload

---
+++ 01_variable_datos.py
@@ -0,0 +1,29 @@
+
+#variables con diferentes tipos de datos
+
+
+nombre = "juan"
+
+
+edad = 25
+
+
+altura = 1.75
+
+
+es_estudiante = True 
+
+
+#Mostrar el tipo de dato de cada variable 
+
+
+print("nombre:", nombre, "tipo de dato:", type (nombre))
+
+
+print("edad:", edad, "tipo de dato:", type (edad))
+
+
+print("altura:", altura, "tipo de dato:", type (altura))
+
+
+print("Es estudiante:", es_estudiante, "tipo de dato:", type (es_estudiante))
